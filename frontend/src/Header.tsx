b1d0745736d87542751cf8c461bbeeb91d54da97
Zineb Iloughmane <ziloughm@c4r8p8.1337.ma>
2023-09-24 21:02:36 +0100

    front-end

--- frontend/src/Header.tsx
+++
@@ -1,8 +0,0 @@
-
-function Header()
-{
-    let name = "zizo";
-    return <header><h1>Helllo {name}</h1></header>;
-}
-
-export default Header
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img src="" alt="" />
+          </div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
